251f3c7d9b67c6d1fd0c7de07ded87f0487c49ae
EC2 Default User <ec2-user@ip-172-16-59-123.ec2.internal>
2019-12-06 21:06:53 +0000

    move to upper level

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import boto3
+import io
+
+s3_client = boto3.client('s3')
+
+def load_csv(s3_client, bucket, key, sep = ','):
+    obj = s3_client.get_object(Bucket=bucket, Key=key)
+    df_data = pd.read_csv(io.BytesIO(obj['Body'].read()),  sep=sep)
+    return df_data
+
+def lambda_handler(event, context):
+    bucket = event['Bucket']
+    key = event['Key']
+    
+    df = load_csv(s3_client, bucket, key)
+    list_cols = list(df.columns)
+    print(list_cols)
+    
+    return list_cols
